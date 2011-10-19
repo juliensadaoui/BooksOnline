9e4b1cc551cc0e9d0617f9d081730130aa482786
CodeZ-Julien <julien.sadaoui@gmail.com>
2011-10-19 14:55:47 +0000

    Ajout du projet EJBService

---
+++ LibraryService/ejbModule/com/insta/fjee/library/service/ILibraryBook.java
@@ -0,0 +1,5 @@
+package com.insta.fjee.library.service;
+
+public interface ILibraryBook {
+
+}
---
+++ LibraryService/ejbModule/com/insta/fjee/library/service/ILibraryService.java
@@ -0,0 +1,7 @@
+package com.insta.fjee.library.service;
+import javax.ejb.Remote;
+
+@Remote
+public interface ILibraryService {
+
+}
