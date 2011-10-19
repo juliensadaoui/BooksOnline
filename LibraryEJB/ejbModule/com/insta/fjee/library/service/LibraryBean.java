6fd51583a1a924ac056a1f89cd0433d00acd44eb
CodeZ-Julien <julien.sadaoui@gmail.com>
2011-10-19 14:55:07 +0000

    Ajout du projet EJB

---
+++ LibraryEJB/ejbModule/com/insta/fjee/library/service/LibraryBean.java
@@ -0,0 +1,20 @@
+package com.insta.fjee.library.service;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class LibraryBean
+ */
+@Stateless
+@LocalBean
+public class LibraryBean implements ILibraryService {
+
+    /**
+     * Default constructor. 
+     */
+    public LibraryBean() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
