fe86fc90a15e0ee3259731f2c534e87b5aa3f826
CodeZ-Julien <julien.sadaoui@gmail.com>
2011-10-20 10:38:43 +0000

    MAJ Exception book

---
+++ LibraryService/ejbModule/com/insta/fjee/library/exception/BookNotFoundException.java
@@ -0,0 +1,29 @@
+package com.insta.fjee.library.exception;
+
+import java.awt.print.Book;
+
+public class BookNotFoundException extends Exception
+{
+
+	private static final long serialVersionUID = 1L;
+
+	private String className;
+	private String isbn;
+
+	public BookNotFoundException(String isbn) {
+		super("Entity of class " + Book.class.getSimpleName() + " with isbn " + isbn + " not found");
+		this.className = Book.class.getSimpleName();
+		this.isbn = isbn;
+	}
+
+	public String getClassName()
+	{
+		return className;
+	}
+
+	public String getIsbn()
+	{
+		return isbn;
+	}
+
+}
