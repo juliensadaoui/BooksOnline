7fbce56ea36ae0ba916d79ffbece7b9128dc2503
CodeZ-Julien <julien.sadaoui@gmail.com>
2011-10-20 15:19:54 +0000

    MAJ testUnitaire

--- LibraryJUnitOutOfServer/src/com/insta/fjee/library/eao/TestBookEAO.java
+++ LibraryJUnitOutOfServer/src/com/insta/fjee/library/eao/TestBookEAO.java
@@ -10,6 +10,7 @@ import javax.persistence.EntityManager;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.insta.fjee.library.entity.Author;
 import com.insta.fjee.library.entity.Book;
 import com.insta.fjee.library.exception.BookNotFoundException;
 import com.insta.fjee.library.junit.Util;
@@ -19,17 +20,67 @@ public class TestBookEAO {
 	private EntityManager _em;
 
 	private BookEAO bookeao;
+	private AuthorEAO authoreao;
 	
     @Before
     public void setUp() throws Exception {
         _em = Util.getEntityManager();
         bookeao = new BookEAO(_em);
+        authoreao = new AuthorEAO(_em);
     }
     
     @Test
     public void bookCountTest() {
         long n = bookeao.countBooks();
         assertEquals(4, n);
+    }
+    
+    @Test
+    public void creatBookTest()
+    {
+    	Book book = new Book();
+    	Author author = new Author();
+    	
+    	author.setFirstName("Yann");
+    	author.setLastName("DUDICOURT");
+    	authoreao.saveOrUpdate(author);
+    	
+    	book.setAuthor(author);
+    	book.setGenre("Test");
+    	book.setIsbn("test01test");
+    	book.setName("Test de yann");
+    	book.setExemplary(1);
+    	
+    	bookeao.saveOrUpdate(book);
+    	assertNotNull(bookeao.findBookByName("Test de yann"));
+    	
+    	bookeao.delete(book);
+    	authoreao.delete(author);
+    	
+    }
+    
+    @Test
+    public void deleatBookTest()
+    {
+    	Book book = new Book();
+    	Author author = new Author();
+    	
+    	author.setFirstName("Yann");
+    	author.setLastName("DUDICOURT");
+    	authoreao.saveOrUpdate(author);
+    	
+    	book.setAuthor(author);
+    	book.setGenre("Test");
+    	book.setIsbn("test01test");
+    	book.setName("Test de yann");
+    	book.setExemplary(1);
+    	bookeao.saveOrUpdate(book);
+    	
+    	bookeao.delete(book);
+    	authoreao.delete(author);
+    	
+    	assertTrue(bookeao.findBookByName("Test de yann").isEmpty());
+    	
     }
     
     @Test
