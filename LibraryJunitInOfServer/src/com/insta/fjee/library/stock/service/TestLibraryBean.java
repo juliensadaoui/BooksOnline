1a4466926e98a98155fabb86b91ae75403d4d322
CodeZ-Julien <julien.sadaoui@gmail.com>
2011-10-29 08:07:57 +0000

    Ajout de nouveaux tests unitaires pour le projet EJB (stock des livres)

--- LibraryJunitInOfServer/src/com/insta/fjee/library/stock/service/TestLibraryBean.java
+++ LibraryJunitInOfServer/src/com/insta/fjee/library/stock/service/TestLibraryBean.java
@@ -22,6 +22,10 @@ import com.insta.fjee.library.stock.service.LibraryBean;
  */
 public class TestLibraryBean 
 {
+	// FIXME 1 - pour les recherches problemes de sensibilité à la casse
+	// FIXME 2 - attention à la contrainte d'intégrité un auteur unique(nom et prenom)
+	// FIXME  - un seul ISBN par livre
+	
 	private static LibraryBean serviceBean;
 	
 	private static List<AuthorDTO> authors;
@@ -41,14 +45,19 @@ public class TestLibraryBean
      */
     public static void setUpResources() 
     {   
-    	String [] authorsList = { "Gustave", "Flaubert",
-    						  "Victor", "Hugo",
-    						  "Jules", "Verne",
-    						  "Henri", "Beyle" };
-    	
     	authors = new ArrayList<AuthorDTO>();
     	books = new ArrayList<BookDTO>();
-    		
+    	
+    	String [] authorsList = { "Gustave", "Flaubert",
+				  "Victor", "Hugo",
+				  "Jules", "Verne",
+				  "Henri", "Beyle",
+				  "Emile", "ZOLA",
+				  "Jean", "Voltaire"};
+    	
+    	/**
+    	 * 	Ajout des auteurs
+    	 */
 		for (int i = 0 ; i < authorsList.length ; i=i+2)
 		{
 			// charge la donnée de base
@@ -57,23 +66,37 @@ public class TestLibraryBean
 			author.setLastName(authorsList[i+1]);
 			authors.add(serviceBean.addAuthor(author));
 		}
-	
+
 		String [] booksList = 
 			{
 				"ZOL569EMI","Le rouge et le noir","ROMAN",
 				"ZOL570EMI","Le tour du monde en 80 jours","ROMAN",
 				"ZOL571EMI","Les misérables","ROMAN",
 				"ZOL572EMI","La Débâcle","ROMAN",
-				"ZOL573EMI","La Terre","ROMAN"
+				"ZOL573EMI","La Terre","ROMAN",
+				"ZOL568EMI","LA CUREE","ROMAN2",
+				"ZOL387EMI","GERMINAL","ROMAN2",
+				"ZOL897EMI","LA DEBACLE","ROMAN2",
+				"VOL765JEA","CANDIDE","ROMAN2"
 			};
-		
-		for (int i = 0 ; i < authorsList.length ; i=i+3)
+
+		/*
+		 * 	Ajout des livres
+		 */
+		for (int i = 0 ; i < booksList.length ; i=i+3)
 		{
+			
 			BookDTO bookDTO = new BookDTO();
 			bookDTO.setIsbn(booksList[i]);
 			bookDTO.setName(booksList[i+1]);
 			bookDTO.setGenre(booksList[i+2]);
-			bookDTO.setAuthorId(authors.get(1).getId());
+			if (i < 15) {
+				bookDTO.setAuthorId(authors.get(1).getId());
+			}
+			else {
+				
+				bookDTO.setAuthorId(authors.get(4).getId());
+			}
 			try {
 				books.add(serviceBean.addBook(bookDTO, 2));
 			} catch (EntityNotFoundException_Exception e) {
@@ -164,6 +187,76 @@ public class TestLibraryBean
 	}
 	
 	/**
+	 * 	Test le service permettant de metttre à jour un auteur dans la
+	 * 		zone de stockage des livres.
+	 */
+	@Test
+	public void updateAuthorTest()
+	{
+		// recupere un auteur dans la liste
+		AuthorDTO authorDTO = null;
+		for (AuthorDTO authorDTOTmp : authors)
+		{
+			if (authorDTOTmp.getLastName().equals("Voltaire"))
+			{
+				authorDTO = authorDTOTmp;
+			}
+		}
+		assertNotNull(authorDTO);
+	
+		
+		// on modifie l'auteur
+		try {
+			authorDTO.setFirstName("Robert");
+			authorDTO.setLastName("Voltaire1");
+			serviceBean.updateAuthor(authorDTO);
+		} catch (EntityNotFoundException_Exception e) 
+		{
+			fail(e.getMessage());
+		}
+
+		List<AuthorDTO> authors = serviceBean.searchAuthorByFirstName("Robert");
+		assertEquals(authors.size(), 1);
+		
+		AuthorDTO author = authors.get(0);
+		assertNotNull(author);
+		assertEquals("Robert", author.getFirstName());
+		assertEquals("Voltaire1", author.getLastName());
+	}
+	
+	/**
+	 * 	Test la méthode permettant de récupérer le nombre d'exemplaire
+	 * 	d'un livre
+	 */
+	@Test
+	public void getExemplaryNumberTest()
+	{
+		long number = serviceBean.getExemplaryNumber("ZOL570EMI");
+		assertEquals(2, number);
+	}
+
+	/**
+	 * 	Test la méthode permettant de récupérer le nombre d'exemplaire
+	 * 	d'un livre avec son isbn
+	 */
+	@Test
+	public void getExemplaryTest()
+	{
+		try {
+			ExemplaryDTO exemplary = serviceBean.getExemplary("ZOL570EMI");
+			assertNotNull(exemplary);
+			
+			assertEquals(2, exemplary.getNb());
+			assertEquals("ZOL570EMI", exemplary.getIsbn());
+			
+		} catch (BookNotFoundException_Exception e) {
+			fail(e.getMessage());
+		}
+
+		
+	}
+	
+	/**
 	 * 	Test de la recherche d'un livre à partir de son ISBN
 	 * 
 	 */
@@ -180,6 +273,111 @@ public class TestLibraryBean
 		} catch (BookNotFoundException_Exception e) {
 			fail(e.getMessage());
 		}
+	}
+	
+	/**
+	 * 	Test la méthode du services web permettant de récuperer une
+	 * 		liste de livres à partir du genre du livre.
+	 */
+	@Test
+	public void searchBookByGenreTestSuccess()
+	{
+		List<BookDTO> books = serviceBean.searchBookByGenre("ROMAN2");
+		assertEquals(books.size(), 4);
+		
+		BookDTO book = books.get(1);
+		assertNotNull(book);
+		assertEquals("ZOL387EMI",book.getIsbn());
+		assertEquals("GERMINAL", book.getName());
+		assertEquals("ROMAN2", book.getGenre());
+		
+		book = books.get(2);
+		assertNotNull(book);
+		assertEquals("ZOL897EMI",book.getIsbn());
+		assertEquals("LA DEBACLE", book.getName());
+		assertEquals("ROMAN2", book.getGenre());
+	}
+	
+	/**
+	 * 	Test la méthode du services web permettant de récuperer une
+	 * 		liste de livres à partir du genre du livre.
+	 */
+	@Test
+	public void searchBookByGenreTestFail()
+	{
+		List<BookDTO> books = serviceBean.searchBookByGenre("POLICIER");
+		assertNotNull(books);
+		assertTrue(books.isEmpty());
+	}
+	
+	/**
+	 * 	Test la méthode du services web permettant de récuperer une
+	 * 		liste de livres à partir du nom du livre
+	 */
+	@Test
+	public void searchBookByNameSuccess()
+	{
+		List<BookDTO> books = serviceBean.searchBookByName("a Terr");
+		assertNotNull(books);
+		assertEquals(books.size(), 1);
+		
+		BookDTO book = books.get(0);
+		assertNotNull(book);
+		assertEquals("ZOL573EMI",book.getIsbn());
+		assertEquals("La Terre", book.getName());
+		assertEquals("ROMAN", book.getGenre());
+	}
+	
+	/**
+	 * 	Test la méthode du services web permettant de récuperer une
+	 * 		liste de livres à partir du nom du livre
+	 * 	Recherche d'un livre qui n'existe pas
+	 */
+	@Test
+	public void searchBookByNameFail()
+	{
+		List<BookDTO> books = serviceBean.searchBookByName("Voyage au centre de la terre");
+		assertNotNull(books);
+		assertTrue(books.isEmpty());
+	}
+	
+	/**
+	 * 	Test la méthode du services web permettant de récuperer une
+	 * 		liste de livres à partir de son auteur
+	 */
+	@Test
+	public void searchBookByAuthorSuccess()
+	{
+		List<BookDTO> books = serviceBean.searchBookByAuthor("Hugo", "Victor");
+		assertNotNull(books);
+		assertEquals(books.size(), 5);
+		
+		BookDTO book = books.get(0);
+		assertNotNull(book);
+		assertEquals("ZOL569EMI",book.getIsbn());
+		assertEquals("Le rouge et le noir", book.getName());
+		assertEquals("ROMAN", book.getGenre());
+		
+		
+		book = books.get(2);
+		assertNotNull(book);
+		assertEquals("ZOL571EMI",book.getIsbn());
+		assertEquals("Les misérables", book.getName());
+		assertEquals("ROMAN", book.getGenre());
+		
+	}
+	
+	/**
+	 * 	Test la méthode du services web permettant de récuperer une
+	 * 		liste de livres à partir de son auteur
+	 * 	Recherche de livres qui n'existent pas
+	 */
+	@Test
+	public void searchBookByAuthorFail()
+	{
+		List<BookDTO> books = serviceBean.searchBookByAuthor("Lamartine", "Alphonse");
+		assertNotNull(books);
+		assertTrue(books.isEmpty());
 	}
 	
 	/**
@@ -223,4 +421,6 @@ public class TestLibraryBean
 		assertEquals(author.getFirstName(), "Victor");
 		assertEquals(author.getLastName(), "Hugo");	
 	}
+	
+
 }
