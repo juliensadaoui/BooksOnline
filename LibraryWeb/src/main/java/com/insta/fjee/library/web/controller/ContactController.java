aabbede6cbd021fa028d44bd2573d7c89ab0634b
CodeZ-Julien <julien.sadaoui@gmail.com>
2011-10-31 12:29:53 +0000

    Résolution du probleme avec svn

--- LibraryWeb/src/main/java/com/insta/fjee/library/web/controller/ContactController.java
+++ LibraryWeb/src/main/java/com/insta/fjee/library/web/controller/ContactController.java
@@ -6,9 +6,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.servlet.HttpServletBean;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.insta.fjee.library.web.bean.Contact;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @SessionAttributes
@@ -16,8 +20,10 @@ public class ContactController {
 
 	@RequestMapping(value = "/addContact", method = RequestMethod.POST)
 	public String addContact(@ModelAttribute("contact")
-							Contact contact, BindingResult result) {
+							Contact contact, BindingResult result,
+							HttpSession session, NativeWebRequest request) {
 		
+//		request.getNativeRequest();
 		System.out.println("First Name:" + contact.getFirstname() + 
 					"Last Name:" + contact.getLastname());
 		
